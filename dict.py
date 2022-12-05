568e23807e2635e8b8cb21e436eaf1b5e9c83389
madisononeil <113647511+madisononeil@users.noreply.github.com>
2022-12-05 11:56:09 -0500

    edits on dictionary file

--- dict.py
+++
@@ -1,3 +0,0 @@
-class Dictionary:
-    def __init__(self) -> None:
-        pass
--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,11 @@
+class Dictionary:
+
+    def __init__(self) -> None:
+        pass
+
+    def words_in_dictionary(file_name):
+        with open(file_name, 'r') as f:
+            dictionary = [word.rstrip() for word in f]
+            all_words= set(dictionary)
+        return all_words
 
